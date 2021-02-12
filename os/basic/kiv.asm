bc620be38436b0e30e369d700e3bb818689d3132
bruneo <39414624+bruneo32@users.noreply.github.com>
2021-02-12 09:17:52 +0100

    Add files via upload

--- os/basic/kiv.asm
+++ os/basic/kiv.asm
@@ -1,32 +1,32 @@
-_PrintString        equ 0x7d00
-_PrintLn            equ 0x7d02
-_PrintStringLn      equ 0x7d04
-_GetPromptString    equ 0x7d08
-_InputBuffer        equ 0x7d0a ; Return de GetPromptString
+_PrintString		equ 0x7d00
+_PrintLn			equ 0x7d02
+_PrintStringLn		equ 0x7d04
+_GetPromptString	equ 0x7d06
+_InputBuffer		equ 0x7d08 ; Return de GetPromptString
 
 global PrintString
 PrintString:
-    mov bx, sp
-    add bx, 4
-    mov si, [bx]
-    call 0:_PrintString
-    ret
+	mov bx, sp
+	add bx, 6
+	mov si, [bx]
+	call 0:_PrintString
+	ret
 
 global PrintLn
 PrintLn:
-    call 0:_PrintLn
-    ret
+	call 0:_PrintLn
+	ret
 
 global PrintStringLn
 PrintStringLn:
-    mov bx, sp
-    add bx, 4
-    mov si, [bx]
-    call 0:_PrintStringLn
-    ret
-    
+	mov bx, sp
+	add bx, 6
+	mov si, [bx]
+	call 0:_PrintStringLn
+	ret
+	
 global GetPromptString
 GetPromptString:
-    call 0:_GetPromptString
-    ; Return a NUL terminated string in _InputBuffer
-    ret
+	call 0:_GetPromptString
+	; Return a NUL terminated string in _InputBuffer
+	ret
