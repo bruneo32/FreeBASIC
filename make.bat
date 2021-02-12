858a150808d2a09c46b7adf15300bfcf22660bc9
arf20 <35542215+arf20@users.noreply.github.com>
2021-02-12 14:02:25 +0100

    Made script more concise

--- make.bat
+++ make.bat
@@ -2,25 +2,10 @@
 Setlocal EnableDelayedExpansion
 echo.
 
-cd os
+nasm os\boot.asm -f bin -o os\boot.bin
+nasm os\main.asm -f bin -o os\main.bin
+nasm os\root.asm -f bin -o os\root.bin
 
-nasm boot.asm -f bin -o boot.bin
-copy /b boot.bin ..\boot.bin
-del boot.bin
-
-nasm main.asm -f bin -o main.bin
-copy /b main.bin ..\main.bin
-del main.bin
-
-nasm root.asm -f bin -o root.bin
-copy /b root.bin ..\root.bin
-del root.bin
-
-cd ..
-
-copy /b boot.bin+main.bin+root.bin bootloader.flp
-del boot.bin
-del main.bin
-del root.bin
+copy /b os\boot.bin + os\main.bin + os\root.bin bootloader.flp
 
 pause
