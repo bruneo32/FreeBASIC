48cf6db4d328751801397288a2508f0e4750c51b
bruneo <39414624+bruneo32@users.noreply.github.com>
2021-02-12 09:18:52 +0100

    Add files via upload

--- os/basic/kiv.asm
+++ os/basic/kiv.asm
@@ -7,7 +7,7 @@ _InputBuffer		equ 0x7d08 ; Return de GetPromptString
 global PrintString
 PrintString:
 	mov bx, sp
-	add bx, 6
+	add bx, 4
 	mov si, [bx]
 	call 0:_PrintString
 	ret
@@ -20,7 +20,7 @@ PrintLn:
 global PrintStringLn
 PrintStringLn:
 	mov bx, sp
-	add bx, 6
+	add bx, 4
 	mov si, [bx]
 	call 0:_PrintStringLn
 	ret
