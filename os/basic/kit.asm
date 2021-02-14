9736a6c5a888655b90f23ebe901b7dd9c9f588d2
bruneo <39414624+bruneo32@users.noreply.github.com>
2021-02-14 23:45:31 +0100

    Add files via upload

--- os/basic/kit.asm
+++ os/basic/kit.asm
@@ -3,15 +3,17 @@ _PrintLn			equ 0x7f02
 _PrintStringLn		equ 0x7f04
 _GetPromptString	equ 0x7f06
 _InputBuffer		equ 0x7f08 ; Return de GetPromptString
-__ensure			equ 0x7f0a
-__more				equ 0x7f0c
+_ClearInputBuffer	equ 0x7f0a
+__ensure			equ 0x7f0c
+__more				equ 0x7f0e
+_ExtendedStore		equ 0x7f10
+_ExtendedLoad		equ 0x7f12
 
 
 global PrintString
 PrintString:
 	mov bx, sp
-	add bx, 6
-	mov si, word [bx] ; Argument 0
+	mov si, word [bx-2] ; Argument 0
 	
 	call word [_PrintString]
 	ret
@@ -24,8 +26,7 @@ PrintLn:
 global PrintStringLn
 PrintStringLn:
 	mov bx, sp
-	add bx, 6
-	mov si, word [bx] ; Argument 0
+	mov si, word [bx-2] ; Argument 0
 	
 	call word [_PrintStringLn]
 	
@@ -36,12 +37,45 @@ GetPromptString:
 	call word [_GetPromptString]
 	ret
 
+global ClearInputBuffer
+ClearInputBuffer:
+	call word [_ClearInputBuffer]
+	ret
+
 global _ensure
 _ensure:
 	call word [__ensure]
+	; Return BL
+	xor ah, ah
+	mov al, bl
 	ret
 
 global _more
 _more:
 	call word [__more]
+	; Return BL
+	xor ah, ah
+	mov al, bl
+	ret
+
+global ExtendedStore
+ExtendedStore:
+	mov bx, sp
+	mov si, word [bx-4] ; Argument 0
+	mov bx, sp
+	xor al, al
+	mov al, byte [bx-2] ; Argument 1
+	
+	call word [_ExtendedStore]
+	ret
+
+global ExtendedLoad
+ExtendedLoad:
+	mov bx, sp
+	mov si, word [bx-2] ; Argument 0
+	
+	call word [_ExtendedLoad]
+	
+	; Return in AL
+	xor ah, ah
 	ret
