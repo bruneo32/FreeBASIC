24bb816c938d781782a251c36f2aa78b7c991494
bruneo <39414624+bruneo32@users.noreply.github.com>
2021-02-18 19:54:24 +0100

    Add files via upload

--- make.bat
+++ make.bat
@@ -4,12 +4,24 @@ echo.
 
 cd os
 
+cd data
+nasm bootdisk.asm -f bin -o bootdisk.bin
+cd ..
+
 nasm boot.asm -f bin -o boot.bin
 nasm main.asm -f bin -o main.bin
+nasm bascore.asm -f bin -o bascore.bin
 nasm root.asm -f bin -o root.bin
+
+
 
 cd ..
 
-copy /b os\boot.bin+os\main.bin+os\root.bin bootloader.flp
+copy /b os\boot.bin+os\main.bin+os\bascore.bin+os\root.bin bootloader.flp
+objdump -D -Mintel,i8086 -b binary -m i386 bootloader.flp>bootloader.debug
 
+del os\boot.bin
+del os\main.bin
+del os\bascore.bin
+del os\root.bin
 pause
