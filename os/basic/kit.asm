77ee5f55e261f4bc95e21c0a19deb7f93c33e31a
bruneo <39414624+bruneo32@users.noreply.github.com>
2021-02-25 13:53:43 +0100

    Add files via upload

--- os/basic/kit.asm
+++ os/basic/kit.asm
@@ -1,80 +1,84 @@
-_PrintString        equ 0x7f00
-_PrintLn            equ 0x7f02
-_PrintStringLn        equ 0x7f04
-_GetPromptString    equ 0x7f06
-_InputBuffer        equ 0x7f08 ; Return de GetPromptString
-_ClearInputBuffer    equ 0x7f0a
-__ensure            equ 0x7f0c
-__more                equ 0x7f0e
-_ExtendedStore        equ 0x7f10
-_ExtendedLoad        equ 0x7f12
+_PrintString			equ 0x1100
+_PrintLn				equ 0x1102
+_PrintStringLn			equ 0x1104
+_GetPromptString		equ 0x1106
+_InputBuffer			equ 0x1108 ; Return de GetPromptString
+_ClearInputBuffer		equ 0x110a
+__ensure				equ 0x110c
+__more					equ 0x110e
+_ExtendedStore			equ 0x1110
+_ExtendedLoad			equ 0x1112
+BasicProgramCounter		equ 0x1114
+MainLoop				equ 0x1116
 
+global _InputBuffer
+global BasicProgramCounter
 
 global PrintString
 PrintString:
-    mov bx, sp
-    mov si, word [bx+2] ; Argument 0
-    
-    call word [_PrintString]
-    ret
+	mov bx, sp
+	mov si, word [bx+2] ; Argument 0
+	
+	call word [_PrintString]
+	ret
 
 global PrintLn
 PrintLn:
-    call word [_PrintLn]
-    ret
+	call word [_PrintLn]
+	ret
 
 global PrintStringLn
 PrintStringLn:
-    mov bx, sp
-    mov si, word [bx+2] ; Argument 0
-    
-    call word [_PrintStringLn]
-    
-    ret
+	mov bx, sp
+	mov si, word [bx+2] ; Argument 0
+	
+	call word [_PrintStringLn]
+	
+	ret
 
 global GetPromptString
 GetPromptString:
-    call word [_GetPromptString]
-    ret
+	call word [_GetPromptString]
+	ret
 
 global ClearInputBuffer
 ClearInputBuffer:
-    call word [_ClearInputBuffer]
-    ret
+	call word [_ClearInputBuffer]
+	ret
 
 global _ensure
 _ensure:
-    call word [__ensure]
-    ; Return BL
-    xor ah, ah
-    mov al, bl
-    ret
+	call word [__ensure]
+	; Return BL
+	xor ah, ah
+	mov al, bl
+	ret
 
 global _more
 _more:
-    call word [__more]
-    ; Return BL
-    xor ah, ah
-    mov al, bl
-    ret
+	call word [__more]
+	; Return BL
+	xor ah, ah
+	mov al, bl
+	ret
 
 global ExtendedStore
 ExtendedStore:
-    mov bx, sp
-    mov si, word [bx+2] ; Argument 0
-    xor ah, ah
-    mov al, byte [bx+4] ; Argument 1
-    
-    call word [_ExtendedStore]
-    ret
+	mov bx, sp
+	mov si, word [bx+2] ; Argument 0
+	xor ah, ah
+	mov al, byte [bx+4] ; Argument 1
+	
+	call word [_ExtendedStore]
+	ret
 
 global ExtendedLoad
 ExtendedLoad:
-    mov bx, sp
-    mov si, word [bx+2] ; Argument 0
-    
-    call word [_ExtendedLoad]
-    
-    ; Return in AL
-    xor ah, ah
-    ret
+	mov bx, sp
+	mov si, word [bx+2] ; Argument 0
+	
+	call word [_ExtendedLoad]
+	
+	; Return in AL
+	xor ah, ah
+	ret
