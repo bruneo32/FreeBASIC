91d4ff6dd5849f1ab48705ca95a3a6431cf8a90d
arf20 <35542215+arf20@users.noreply.github.com>
2021-03-04 19:38:46 +0100

    Delete os/basic directory

--- os/basic/disassembly.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-Setlocal EnableDelayedExpansion
-echo.
-
-objdump -D -Mintel,i8086 -b binary -m i386 core.bin>core.debug
-
-REM pause
--- os/basic/perm.sh
+++
@@ -1,6 +0,0 @@
-chmod +rw linker.ld
-chmod +rw linkerelf.ld
-chmod +rw entry.asm
-chmod +rw kit.asm
-chmod +rw main.c
-chmod +xrw make.sh
